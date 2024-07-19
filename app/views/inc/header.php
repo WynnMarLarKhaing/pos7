af9c57925b0ea4e30a48536e0bfd6ada6e913974
WynnMarLarKhaing <wynnmarlarkhaing.1993@gmail.com>
2024-07-19 22:46:23 +0900

    fix connection time out error

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -8,9 +8,9 @@
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/customer_style.css">
     <link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/css/datatables.min.css"/>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <script src="<?php echo URLROOT; ?>/js/jquery_1.9.1.min.js"></script>
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/font-awesome.min.css">
     <script type="text/javascript" src="<?php echo URLROOT; ?>/js/datatables.min.js" defer></script>
     <title><?php echo SITENAME; ?></title>
 </head>
